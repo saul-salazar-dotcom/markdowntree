0833c9311ff30dba315c5f47cacba363de9c72d5
Saul Salazar <saulmmendoza+github@gmail.com>
2025-07-28 01:18:39 +0000

    feat: force updates on service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'markdowntree-v1';
+const CACHE_NAME = 'markdowntree-v2';
 const urlsToCache = ['/'];
 
 self.addEventListener('install', event => {
@@ -6,6 +6,15 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
